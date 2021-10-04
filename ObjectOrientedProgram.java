ff721f6468b4a5dfeb902ba07392ce2376c2192b
unknown <rajputnikita5666@gmail.com>
2021-10-04 15:19:00 +0530

    Added:Welcome Object oriented program

---
+++ ObjectOrientedProgram.java
@@ -0,0 +1,10 @@
+package com.bz.objectoriented;
+
+public class ObjectOrientedProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Object Oriented Program");
+
+	}
+
+}
